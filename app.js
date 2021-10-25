888a3ace1e0e381bf0e21df476c0723a049e7df7
MKougia <kougia.mihkel@khk.ee>
2021-10-25 17:54:22 +0300

    Array and Methods

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
-const name = "Anna Karutina";
-const age = 40;
-const job = "Kutseõpetaja";
-const city = "Tartu";
-
-let html;
-
-// es5
-
-html = '<ul>' +
-			'<li>' + name + '</li>' +
-			'<li>' + age + '</li>' +
-			'<li>' + job + '</li>' +
-			'<li>' + city + '</li>' +
-		'</ul>';
+const numbers1 = [43, 56, 33, 23, 44, 36, 5];
+const numbers2 = new Array(22, 45, 33, 76, 54);
+const fruits = ['õun', 'banaan', 'apelsiin', 'virsik'];
+const mixed = [22, 'Tere', true, undefined, null, {a:1, b:2}, new Date()];
 
 
-// es6 - template string
+let val;
 
-html = `
-	<ul>
-		<li>Name: ${name}</li>
-		<li>Age: ${age}</li>
-		<li>Job: ${job}</li>
-		<li>City: ${city}</li>
-	</ul>
-`;
+val = numbers1.length; // massiivi pikkus
+ 
+val = Array.isArray(numbers1); // kas on massiiv
 
-console.log(html);
+val = numbers1[3]; // kindel element
+val = numbers1[0];
 
-document.body.innerHTML = html;
+numbers1[2] = 100; // elemendi lisamine
+
+val = numbers1.indexOf(36); // elemendi järgi index
+
+numbers1.push(250); // element massiivi lõppu
+numbers1.unshift(120); // element massiivi ette
+numbers1.pop(); // element massiivi lõpust maha
+numbers1.shift(); // element massiivi eest maha
+
+// console.log(numbers1);
+
+// numbers1.splice(1, 3); // elementide välja lõikamine
+
+// numbers1.reverse(); // targurpidi massiiv
+val = numbers1.concat(numbers2); // massivide ühendamine
+
+val = fruits.sort(); // teksti masiivi sorteerimine
+
+console.log(numbers1);
+
+// numbrite massiivi sorteerimine kasvujärjekorras
+val = numbers1.sort(function(x, y){
+	return x - y;
+});
+
+// numbrite massiivi sorteerimine kahanemisjärjekorras
+val = numbers1.sort(function(x, y){
+	return y - x;
+});
+
+console.log(val);
