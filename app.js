f2dce2f99cb6ce88908c3764c361a55105741125
MKougia <kougia.mihkel@khk.ee>
2021-10-25 18:11:55 +0300

    Functions

--- app.js
+++ app.js
@@ -1,68 +1,50 @@
-// for tsükkel
-
-console.log('for tsükkel');
-for(let i = 0; i < 10; i++){
-	if(i == 5){
-		// break;
-		continue;
+// funktsiooni defineerimine (deklareerimine)
+function greeting(firstname = 'Kadi', lastname = 'Tamm'){
+	/*
+	if(typeof firstname === 'undefined'){
+		firstname = 'Kadi';
 	}
-	console.log(i);
+	if(typeof lastname === 'undefined'){
+		lastname = 'Tamm';
+	}
+	*/
+	return 'Tere, ' + firstname + ' ' + lastname;
 }
+// funktsiooni kutsumine tööle
 
-// while tsükkel
-
-console.log('while tsükkel');
-
-let i = 0;
-while(i < 10){
-	console.log(i);
-	i++;
-}
-
-// do while
-
-console.log('do while tsükkel');
-
-let j = 0;
-do {
-	console.log(j);
-	j++;
-} while(j < 10);
-
-// tsüklid ja massiivid
-
-console.log('massiivid ja tsüklid');
-const cars = ['Ford', 'Honda', 'Toyota'];
-
-// for tsükliga
-for(let i = 0; i < cars.length; i++){
-	console.log(cars[i]);
-}
+let greet = greeting('Anna', 'Karutina');
+console.log(greet);
 
 console.log();
+// funktsioon avaldise kujul
+const square = function(number = 3){
+	return number * number;
+}
+console.log(square());
 
-// forEach - callback function
-cars.forEach(function(element, index, array){
-	console.log(`cars[${index}] = ${element}`);
-	console.log(array);
-});
+// IIFE - Immidiatley Invokable Function Expression 
+(function(){
+	console.log('function is ran');
+})();
 
-// forEach - (arrow) => function
-cars.forEach((element, index) => {
-	console.log(`cars[${index}] = ${element}`);
-});
-
+(function(name){
+	console.log('Tere ' + name);
+})('Anna');
 
 console.log();
-
-// tsüklid ja objektid
-const person = {
-	firstname: 'Kadi',
-	surname: 'Tamm',
-	age: 25
+// objekti see defineeritud funktsioonid
+const todo = {
+	add: function(){
+		console.log('add todo ...');
+	},
+	edit: function(id){
+		console.log(`edit todo nr ${id} ...`);
+	},
+	delete: function(){
+		console.log('delete todo ...');
+	}
 }
 
-// for in tsükkel
-for(let key in person){
-	console.log(`${key} = ${person[key]}`);
-}
+todo.add();
+todo.edit(5);
+todo.delete();
