f3b3e409ef9b630a2301fed716d1e07bb5c01dad
MKougia <kougia.mihkel@khk.ee>
2021-10-25 18:04:54 +0300

    Condition statemen - If

--- app.js
+++ app.js
@@ -1,26 +1,75 @@
-let val;
+/*
+if(tingimus) {
+	// tegevused if puhul
+} else {
+	// tegevused else puhul
+}
+*/
 
-const today = new Date();
+const id = 100;
 
-val = today.getMonth(); // 0 - jaanuar
-val = today.getDate();
-val = today.getDay();
-val = today.getFullYear();
-val = today.getHours();
-val = today.getMinutes();
-val = today.getSeconds();
-val = today.getMilliseconds();
-val = today.getTime();
+// == - võrdub
+// != - ei võrdu
+// === - võrdub ja tüübikontroll
+// !== - ei võrdu ja tüübikontroll
 
-let birthday = new Date("09/27/2001");
-birthday = new Date('September 27 2001');
+// kui undefined
+/*
+let testId;
+if(typeof testId !== 'undefined'){
+	console.log(`id on ${testId}`);
+} else {
+	console.log('id puudub');
+}
+*/
 
-birthday.setMonth(2); // 0 - jaanuar
-birthday.setDate(12);
-birthday.setFullYear(2002);
-birthday.setHours(11);
-birthday.setMinutes(25);
-birthday.setSeconds(50);
+// > ja <
+/*
+if(id > 100) {
+	console.log(`id on ${id} - suurem kui 100`);
+} else {
+	console.log(`id on ${id} - väiksem või võrdne 100`);
+}
+*/
 
-console.log(birthday);
-console.log(val);
+// if - else if - else
+/*
+const color = "blue";
+if(color === 'red'){
+	console.log('Stop!');
+} else if(color === 'yellow'){
+	console.log('Attention!');
+} else if(color === 'green'){
+	console.log('Go!');
+} else {
+	console.log('Color is not accepted!');
+}
+*/
+
+// and - &&
+const name = 'Kadi';
+const age = 70;
+
+if(age > 0 && age <= 12){
+	console.log(`${name} on laps`);
+} else if(age > 12 && age < 18){
+	console.log(`${name} on nooruk`);
+} else {
+	console.log(`${name} on täiskasvanu`);
+}
+
+// or - ||
+if(age < 18 || age > 65){
+	console.log(`${name} ei saa registreerida`);
+} else {
+	console.log(`${name} on registreeritud`);
+}
+
+// ? - ternary operaator
+console.log(id === 100 ? 'Õige' : 'Vale');
+
+// ilma {}
+if(id === 100) 
+	console.log('Korras');
+else 
+	console.log('Ei ole korras');
