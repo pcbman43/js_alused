a2354a7df7dfddbe138b4d53902257ad3ce77a7c
MKougia <kougia.mihkel@khk.ee>
2021-10-25 18:31:44 +0300

    DOM element creation

--- app.js
+++ app.js
@@ -1,36 +1,21 @@
-let val;
+// elemendi loomine
+const li = document.createElement('li');
 
 const list = document.querySelector('ul');
-const listItem = document.querySelector('li:first-child');
+// klassi maaramine
+li.className = 'collection-item';
 
-val = list;
-val = listItem;
+// teksti lisamine
+li.appendChild(document.createTextNode('Study element creation'));
 
-val = list.childNodes;
-val = list.childNodes[0];
-val = list.childNodes[0].nodeName;
-val = list.childNodes[0].nodeType;
+const link = document.createElement('a');
+link.className = 'secondary-content';
+link.appendChild(document.createTextNode('X'));
 
-val = list.children;
-val = list.children[1];
-list.children[1].textContent = 'Study XML';
+// lisa atribuut vaartusega
+link.setAttribute('href', '#');
 
-val = list.children[2].children;
-list.children[2].children[0].id = 'test-id';
+li.appendChild(link);
 
-val = list.firstChild;
-val = list.firstElementChild;
-
-val = list.lastChild;
-val = list.lastElementChild;
-
-val = list.childElementCount;
-
-val = listItem.parentNode;
-val = listItem.parentElement;
-val = listItem.parentElement.parentElement;
-
-val = listItem.nextSibling;
-val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
-
-console.log(val);
+// elemendi lisamine teise elemendi sisse
+list.appendChild(li);
