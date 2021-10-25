976d319b3683a331b5ea8ae9f8e46caf52d3cc83
MKougia <kougia.mihkel@khk.ee>
2021-10-25 18:29:37 +0300

    DOM selectors of multple events

--- app.js
+++ app.js
@@ -1,39 +1,15 @@
 let val;
 
-// document.getElementById();
+val = document.querySelectorAll('li');
 
-val = document.getElementById('task-title');
-val = document.getElementById('task-title').id;
-val = document.getElementById('task-title').className;
+const oddLi = document.querySelectorAll('li:nth-child(odd)');
 
-const taskTitle = document.getElementById('task-title');
+const evenLi = document.querySelectorAll('li:nth-child(even)');
 
-// stiili muutmine
+oddLi.forEach(function(li){
+	li.style.background = '#ddd';
+});
 
-taskTitle.style.background = '#333';
-taskTitle.style.color = '#fff';
-taskTitle.style.padding = '5px';
-// taskTitle.style.display = 'none';
-
-// sisu muutmine
-taskTitle.textContent = 'My Tasks';
-taskTitle.innerText = 'My Favorite Tasks';
-taskTitle.innerHTML = '<span style="color: red;">My Tasks</span>';
-
-// document.querySelector();
-
-val = document.querySelector('#task-title');
-val = document.querySelector('.card-title');
-val = document.querySelector('h2');
-
-document.querySelector('li').style.color = 'green';
-
-document.querySelector('ul li').style.color = 'blue';
-
-document.querySelector('li:last-child').style.color = 'green';
-
-document.querySelector('li:nth-child(odd)').style.background = '#ccc';
-document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
-document.querySelector('li:last-child').style.background = '#ccc';
-
-console.log(val);
+for(let i = 0; i < evenLi.length; i++){
+	evenLi[i].style.background = '#f4f4f4';
+}
