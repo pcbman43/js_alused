f142d26b605b3d895c75be4171e48ce352558046
MKougia <kougia.mihkel@khk.ee>
2021-10-25 18:43:33 +0300

    Delete single element

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 // event elements
-const form = document.querySelector('form');
-const taskInput = document.querySelector('#task');
+const taskList = document.querySelector('ul');
 
-// submit
-form.addEventListener('submit', addTask);
 
-function addTask(e) {
-	const li = document.createElement('li');
-	const list = document.querySelector('ul');
-	
-	li.className = 'collection-item';
-	li.appendChild(document.createTextNode(taskInput.value));
+// click elemendi kustutamiseks
+taskList.addEventListener('click', deleteTask);
 
-	const link = document.createElement('a');
-	link.className = 'secondary-content';
-	link.appendChild(document.createTextNode('X'));
-	link.setAttribute('href', '#');
-
-	li.appendChild(link);
-	list.appendChild(li);
-
-	taskInput.value = '';
-
-	e.preventDefault();
+function deleteTask(e) {
+	if(e.target.textContent == 'X'){
+		if(confirm('Are you sure to delete this task?')){
+			e.target.parentElement.remove();
+		}
+	}
 }
